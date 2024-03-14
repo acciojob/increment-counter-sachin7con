b035fa8d04ee6b49500a369a1ea3d4e547ccdf04
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-14 11:03:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+//SGN your JS code here. If required.
+let counter = document.getElementById("count");
+
+function countFunc() {
+	
+	counter.innerHTML = parseInt(counter.innerHTML) +1;
+}
